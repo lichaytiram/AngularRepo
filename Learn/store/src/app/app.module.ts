3678f92f78fb13361cfcaf434c29abcf3706eac3
lichaytiram <lichaytiram@gmail.com>
2020-01-05 22:22:15 +0200

    web

--- Learn/store/src/app/app.module.ts
+++ Learn/store/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { EffectsModule } from '@ngrx/effects';
 import { reducers, effects } from './store';
 
 import { AppRoutingModule } from './app-routing.module';
+import { environment } from '../environments/environment';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { AddComponent } from './add/add.component';
@@ -35,6 +37,11 @@ import { ReadComponent } from './read/read.component';
         strictStateSerializability: true,
         strictActionSerializability: true,
       },
+    }),
+
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, // Retains last 25 states
+      logOnly: environment.production, // Restrict extension to log-only mode
     }),
 
   ],
