a74874996dce2e37642d2f64342b456b6fb1a815
lichaytiram <lichaytiram@gmail.com>
2020-03-01 02:31:50 +0200

    web

--- Projects/ProteinNutrition/src/app/home/home.component.ts
+++ Projects/ProteinNutrition/src/app/home/home.component.ts
@@ -33,7 +33,7 @@ export class HomeComponent implements OnInit, OnDestroy {
 
   ngOnDestroy() {
 
-    this.clearInterval.forEach(id => clearInterval(id))
+    this.clearInterval.forEach(id => clearInterval(id));
 
   }
 
