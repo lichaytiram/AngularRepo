aa140813fce300b84872471d9370c79eb4942267
lichaytiram <lichaytiram@gmail.com>
2020-03-11 02:20:09 +0200

    project

--- Projects/ProteinNutrition/dist/ProteinNutrition/styles-es2015.js
+++ Projects/ProteinNutrition/dist/ProteinNutrition/styles-es2015.js
@@ -338,7 +338,7 @@ if (content.locals) {
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! c:\Users\Lichay\gitStudio\AngularRepo\Projects\ProteinNutrition\src\styles.css */"./src/styles.css");
+module.exports = __webpack_require__(/*! C:\Users\Lichay\gitStudio\AngularRepo\Projects\ProteinNutrition\src\styles.css */"./src/styles.css");
 
 
 /***/ })
