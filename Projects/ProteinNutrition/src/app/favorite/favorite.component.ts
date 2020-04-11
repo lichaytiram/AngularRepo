ac2e11d554c15d2801722a334136c22906dc23ee
lichaytiram <lichaytiram@gmail.com>
2020-04-11 12:36:38 +0300

    project

--- Projects/ProteinNutrition/src/app/favorite/favorite.component.ts
+++ Projects/ProteinNutrition/src/app/favorite/favorite.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Store, select } from '@ngrx/store';
+import { IProductsState } from '../store';
+
+import { getUser, getAllProteins } from '../store/selectors'
+import { Observable } from 'rxjs';
+import { IUser } from '../shared/models/iUser.model';
+import { IProtein } from '../shared/models/iProtein.model';
 
 @Component({
   selector: 'app-favorite',
@@ -8,21 +15,20 @@ import { Router } from '@angular/router';
 })
 export class FavoriteComponent implements OnInit {
 
-  public login: boolean;
+  public user$: Observable<IUser>
+  public proteins$: Observable<IProtein[]>
 
-  constructor(private router: Router) { }
+  constructor(private store: Store<IProductsState>, private router: Router) { }
 
   ngOnInit() {
 
-    let isLogin: boolean = !!sessionStorage.getItem("login");
-    this.login = isLogin ? true : false;
+    this.user$ = this.store.pipe(select(getUser));
+    this.proteins$ = this.store.pipe(select(getAllProteins));
 
   }
 
   public loginPage(): void {
     this.router.navigate(['/product/login']);
   }
-
-
 
 }
