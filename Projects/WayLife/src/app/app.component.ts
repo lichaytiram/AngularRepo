06faaf91056ec275b7fa4ad0e6fbfd7b91846bea
lichaytiram <lichaytiram@gmail.com>
2020-11-23 15:46:57 +0200

    project

--- Projects/WayLife/src/app/app.component.ts
+++ Projects/WayLife/src/app/app.component.ts
@@ -49,7 +49,7 @@ export class AppComponent implements OnInit {
       return;
     }
 
-    const pattrn = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
+    const pattrn: RegExp = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
     const regex = new RegExp(pattrn);
     if (!regex.test(user.email)) {
       this.inputValid = false;
@@ -68,76 +68,48 @@ export class AppComponent implements OnInit {
 
     gsap.registerPlugin(ScrollTrigger);
 
-    gsap.timeline({
-      scrollTrigger: {
-        trigger: '.container',
-        start: 'center center',
-        end: '+=2300',
-        scrub: true,
-        // markers: true,
-        pin: true
-      }
-    })
-      .to('.outImg', { opacity: 0 })
-      .from('.container .container1 .right', { x: innerWidth * 1, opacity: 0 }, "-=0.6")
-      .from('.right h2', { y: 100, opacity: 0 })
-      .to('.right h2', { y: 50, scale: 0, opacity: 0 })
-      .from('.right h3', { y: 100, x: 30, opacity: 0, scale: 0 })
-      .from('.right .smallImg', { y: -100, opacity: 0, scale: 0 })
+    this.imgContainer();
+    this.parallaxContainer();
 
-    gsap.utils.toArray('.space .flex>img').forEach((element: HTMLElement) => {
+  }
+
+  private imgContainer(): void {
+
+    const flex1 = gsap.timeline({
+      scrollTrigger: {
+        trigger: '.container .walk',
+        start: 'bottom 98%',
+        end: 'center top',
+        toggleActions: 'play reverse play reverse'
+      }
+    });
+    const flex2 = gsap.timeline({
+      scrollTrigger: {
+        trigger: '.container .seaAtNight',
+        start: 'bottom 98%',
+        end: 'center top',
+        toggleActions: 'play reverse play reverse'
+      }
+    });
+
+    flex1
+      .from('.walk', { x: -300, opacity: 0, duration: .8, ease: 'Power0.easeNone' })
+      .from('.yoga', { x: 300, opacity: 0, duration: .8, ease: 'Power0.easeNone' }, "-=");
+    flex2
+      .from('.seaAtNight', { x: -300, opacity: 0, duration: 1.1, ease: 'Power0.easeNone' })
+      .from('.elephant', { x: 300, opacity: 0, duration: 1.1, ease: 'Power0.easeNone ' }, "-=");
+
+    gsap.utils.toArray('.imgContainer .flex>img').forEach((element: HTMLElement) => {
       const spaceHover = gsap.to(element, { scaleX: 1.12, scaleY: 1.12, scaleZ: 1.5, duration: 1.2, paused: true, ease: "ease-in-out" });
       element.addEventListener('mouseenter', () => spaceHover.play());
       element.addEventListener('mouseleave', () => spaceHover.reverse());
     });
 
-    // const space = document.querySelector('.space .flex>img');
-
-    // space.addEventListener("mouseenter", () => spaceHover.play());
-    // space.addEventListener("mouseleave", () => spaceHover.reverse());
-
-    // const space = document.querySelector('.space .flex>img');
-    // const spaceHover = gsap.to(space, {
-    //   scaleX: 1.12, scaleY: 1.12, scaleZ: 1.5,
-    //   color: "blue",
-    //   duration: 1.5,
-    //   paused: true,
-    //   ease: "ease-in-out"
-    // });
-
-    // space.addEventListener("mouseenter", () => spaceHover.play());
-    // space.addEventListener("mouseleave", () => spaceHover.reverse());
-
-
-    const flex1 = gsap.timeline({
-      scrollTrigger: {
-        trigger: '.container2 .walk',
-        start: 'bottom 90%',
-        toggleActions: 'restart none none reverse'
-      }
-    });
-    const flex2 = gsap.timeline({
-      scrollTrigger: {
-        trigger: '.container2 .seaAtNight',
-        start: 'bottom 90%',
-        toggleActions: 'restart none none reverse'
-      }
-    });
-
-    flex1
-      .from('.walk', { x: -300, opacity: 0, duration: 4 })
-      .from('.yoga', { x: 300, opacity: 0, duration: 4 }, "-=");
-    flex2
-      .from('.seaAtNight', { x: -300, opacity: 0, duration: 4 })
-      .from('.treesIsland', { x: 300, opacity: 0, duration: 4 }, "-=");
-
-    this.parallax();
-
   }
 
-  private parallax(): void {
+  private parallaxContainer(): void {
 
-    const totalScroll = 3000;
+    const totalScroll = 1500;
 
     gsap.to(".parallaxContainer", {
       scrollTrigger: {
@@ -148,8 +120,8 @@ export class AppComponent implements OnInit {
 
     gsap.to(".parallax-bg", {
       scrollTrigger: {
-        trigger: ".parallax-bg",
-        start: 'center center',
+        trigger: ".parallaxContainer",
+        start: 'top 40%',
         end: `+=${totalScroll}`,
         scrub: true
       },
