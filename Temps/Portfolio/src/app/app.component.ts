4328eb9979b1d562a9ac2a5669d2937e201db445
lichaytiram <lichaytiram@gmail.com>
2021-01-06 15:36:01 +0200

    projects

--- Temps/Portfolio/src/app/app.component.ts
+++ Temps/Portfolio/src/app/app.component.ts
@@ -1,13 +1,61 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Portfolio';
 
+  ngOnInit() {
 
+    this.gsap();
+
+  }
+
+  private gsap(): void {
+
+    gsap.registerPlugin(ScrollTrigger);
+
+    this.container();
+
+  }
+
+  private container(): void {
+
+    const selector: string = ".container>section>.textContainer>";
+
+    gsap.utils.toArray(`${selector}.information`).forEach((targetElement: HTMLElement) => {
+      gsap.to(targetElement, {
+        scrollTrigger: {
+          trigger: targetElement, start: '0 85%', toggleActions: 'play reverse play reverse'
+        },
+        y: '0', duration: '.7', ease: 'none', opacity: '1'
+      });
+    });
+
+    gsap.utils.toArray(`${selector}.purpose`).forEach((targetElement: HTMLElement) => {
+      gsap.to(targetElement, {
+        scrollTrigger: {
+          trigger: targetElement, start: '0 85%', toggleActions: 'play reverse play reverse'
+        },
+        y: '0', duration: '.7', ease: 'none', opacity: '1'
+      });
+    });
+
+    gsap.utils.toArray(`${selector}.links`).forEach((targetElement: HTMLElement) => {
+      gsap.to(targetElement, {
+        scrollTrigger: {
+          trigger: targetElement, start: '0 92%', toggleActions: 'play reverse play reverse'
+        },
+        y: '0', duration: '.7', ease: 'none', opacity: '1'
+      });
+    });
+
+  }
 
 }
